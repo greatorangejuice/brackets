c11aac9854131d58b7a0b20a21610fe44047b49b
greatorangejuice <bypavelsnigirev@gmail.com>
2019-03-21 19:13:40 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,49 +1,65 @@
-// module.exports =
-function check(str, bracketsConfig) {
-  if (!(str.length % 2 == 0)) {
-    return false
-  }
-  // concat array for find open and close brackets 
-  let newArray = bracketsConfig.reduce(
-    (result, element) => result.concat(element), []
-  )
-  console.log(`after concat: `, newArray)
-
-  let openSymbols = [];
-  let closeSymbols = [];
-  newArray.forEach((item, index) => {
-    if (index % 2 == 0) {
-      openSymbols.push(item)
-    } else {
-      closeSymbols.push(item)
+module.exports =
+  function check(str, bracketsConfig) {
+    if (!(str.length % 2 == 0)) {
+      return false
     }
-  });
-  console.log("Открывающие скобки " + openSymbols)
-  console.log("Закрывающие скобки " + closeSymbols)
+    // concat array for find open and close brackets 
+    let newArray = bracketsConfig.reduce(
+      (result, element) => result.concat(element), []
+    )
+    console.log(`after concat: `, newArray)
 
-  let stack = [];
-  for (let i = 0; i < str.length; i++) {
-    // Check our inner symbol. Should be open
-    if (openSymbols.includes(str[i])) {
-      stack.push(str[i]);
-    } else {
-      stack.pop();
+    let openSymbols = [];
+    let closeSymbols = [];
+    newArray.forEach((item, index) => {
+      if (index % 2 == 0) {
+        openSymbols.push(item)
+      } else {
+        closeSymbols.push(item)
+      }
+    });
+    console.log("Открывающие скобки " + openSymbols)
+    console.log("Закрывающие скобки " + closeSymbols)
+
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+      // console.log(`Close symb: `,closeSymbols.indexOf(str[i]), `>>>>`,str[i],`<<<<`);
+      // console.log(`Open symb: `,openSymbols.indexOf(str[i-1]), `>>>>`,str[i-1],`<<<<`);
+      // Check our inner symbol. Should be open
+      if (openSymbols.includes(str[i])) {
+        stack.push(str[i]);
+      } else if (openSymbols.indexOf(stack[stack.length - 1]) == closeSymbols.indexOf(str[i])) {
+        stack.pop();
+      }
+      if (openSymbols.includes(str[i]) && closeSymbols.includes(str[i]) && stack[stack.length - 2] == stack[stack.length - 1]) {
+        stack.pop();
+        stack.pop();
+        // stack.splice(-0, 2)
+      }
+      console.log(`stack: `, stack)
     }
+    if (stack.length == 0) {
+      return true
+    } else {
+      return false
+    }
+
   }
-  if (stack.length == 0) {
-    return true
-  } else {
-    return false
-  }
 
-}
+  // console.log(check('|()|(||)||', [
+  //   ['(', ')'],
+  //   ['|', '|']
+  // ]));  
 
-console.log(check('[(])', [
-  ['(', ')'],
-  ['[', ']']
-]));
+// console.log(check('[(])', [
+//   ['(', ')'],
+//   ['[', ']']
+// ]));
 
+// console.log(check('||||', [
+//   ['|', '|']
 
+// ]));
 
 // var arrays = [
 //   ['(', ')'],
